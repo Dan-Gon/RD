af7091538dd407c59223095c0c0d6789846e03d4
dan <itdudeislit@gmail.com>
2022-08-07 13:53:52 -0500

    Updated NavBar

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+import './Header.css';
+
+const Header = () => {
+  return(
+      <div className={"header-container"}>
+        <nav className={"nav-links"}>
+          <h1>RecoveryDiscovery</h1>
+          <Link className={"links"} to="/">Home</Link>
+          <Link className={"links"} to="/about">About</Link>
+          <Link className={"links"} to="/people">People</Link>
+        </nav>
+      </div>
+  )
+}
+export default Header;
---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header/Header";
+import './Home.css'
+
+const Home = () => {
+  return(
+      <div>
+        <Header/>
+      <section className={'home-body'}>
+      </section>
+      <section className={'section2'}>
+
+      </section>
+      </div>
+  )
+}
+export default Home;
