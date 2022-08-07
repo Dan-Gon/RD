8cc6bd4e7bcb3576bbd04cb1a113a983eca92092
dan <itdudeislit@gmail.com>
2022-08-07 18:58:01 -0500

    idk

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,14 +1,21 @@
 import Header from "../../components/Header/Header";
 import './Home.css'
+import {useEffect, useState} from "react";
 
 const Home = () => {
+    const [data, setData] = useState(null)
+    useEffect(()=>{
+        fetch("/api")
+            .then((res)=>res.json())
+            .then((data)=> setData(data.message))
+    },[])
   return(
       <div>
         <Header/>
       <section className={'home-body'}>
       </section>
       <section className={'section2'}>
-
+        <h1>{data}</h1>
       </section>
       </div>
   )
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/api", (req,res)=>{
+    res.json({message: "Hello"})
+})
 app.listen(4000, ()=>{
     console.log("Server initiated");
 })
